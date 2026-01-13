f61e8a26d0d1871d499413862004b5baa1555deb
Ashutosh Joshi <joshiashutosh9410773594@gmail.com>
2026-01-13 23:25:26 +0530

    minor correction

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -171,7 +171,7 @@ BEGIN
         PRINT '>> Inserting Data Into: silver.erp_loc_a101';
         INSERT INTO silver.erp_loc_a101 (cid, cntry)
         SELECT
-            cid,
+            REPLACE(cid, '-', '') AS cid,
             CASE 
                 WHEN TRIM(cntry) = 'DE' THEN 'Germany'
                 WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
